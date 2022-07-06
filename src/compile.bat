d10f434033d2c27f269325d9f5e0b277729efa8c
Ashwin Kalyan <64381851+Ashwin-Kalyan@users.noreply.github.com>
2022-07-06 17:10:05 -0500

    Add files via upload

---
+++ src/compile.bat
@@ -0,0 +1,3 @@
+cd "C:\Users\Ashwin\Desktop\Programming Portfolio\Python\bmiCalc\src"
+pip install pyinstaller
+pyinstaller --onefile --noconsole --icon="C:\\Users\\Ashwin\\Desktop\\Programming Portfolio\\Python\\bmiCalc\\icon\\favicon.ico" -w "main.py"
