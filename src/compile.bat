c2787904ba2778b5690c2eba3ecd7997bb4ba621
Ashwin Kalyan <64381851+Ashwin-Kalyan@users.noreply.github.com>
2022-08-27 01:10:12 -0500

    Delete src directory

--- src/compile.bat
+++
@@ -1,3 +0,0 @@
-cd "C:\Users\Ashwin\Desktop\Programming Portfolio\Python\bmiCalc\src"
-pip install pyinstaller
-pyinstaller --onefile --noconsole --icon="C:\\Users\\Ashwin\\Desktop\\Programming Portfolio\\Python\\bmiCalc\\icon\\favicon.ico" -w "main.py"
